6e259f98128b7accd8320a5fce0206ab4a33f3c4
Gearlee97 <and9710@gmail.com>
2025-12-04 01:23:21 +0000

    feat: enable globals.css and test Tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import "./globals.css"; // <- penting: panggil global css (tailwind)
 
 export const metadata = {
   title: "Mintrias",
-};
+    description: "Mintrias App",
+    };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" translate="no">
-      <head>
-        <meta name="google" content="notranslate" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-}
+    export default function RootLayout({ children }: { children: React.ReactNode }) {
+      return (
+          <html lang="en" translate="no">
+                <head>
+                        <meta name="google" content="notranslate" />
+                              </head>
+                                    <body className="bg-background text-foreground">
+                                            {children}
+                                                  </body>
+                                                      </html>
+                                                        );
+                                                        }
