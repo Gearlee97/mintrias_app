9c1f6c13c4fc74db1d420569ee242f0bdced039c
Gearlee97 <and9710@gmail.com>
2025-12-04 00:07:36 +0000

    feat: add RootLayout to disable translation & fix hydration mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" translate="no">
       <head>
-        {/* cegah Google/Chrome auto-translate */}
         <meta name="google" content="notranslate" />
       </head>
       <body>{children}</body>
